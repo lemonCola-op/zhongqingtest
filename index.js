3490ca6e531982d6c752a6a758fa44a477134260
lemonCola-op <75073017+lemonCola-op@users.noreply.github.com>
2021-01-09 18:08:27 +0800

    Update index.js

--- index.js
+++ index.js
@@ -33,8 +33,8 @@ exports.main_handler = async (event, context, callback) => {
 'use strict';
 exports.main_handler = async (event, context, callback) => {
   //解决云函数热启动问题
-  delete require.cache[require.resolve('./qqreadbox.js')];
-  require('./qqreadbox.js') //这里写你想要的脚本
+  delete require.cache[require.resolve('./youth.js')];
+  require('./youth.js') //这里写你想要的脚本
   //require('./jd_xtg2.js') //这里写你想要的脚本
   //require('./jd_xtg3.js') //这里写你想要的脚本
 }
