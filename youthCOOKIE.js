b785d16145992a50df6b221e1471c160620c0331
lemonCola-op <75073017+lemonCola-op@users.noreply.github.com>
2021-01-12 21:57:44 +0800

    Add files via upload

---
+++ youthCOOKIE.js
@@ -0,0 +1,30 @@
+
+
+//独立COOKIE文件     ck在``里面填写，多账号换行
+let qqreadbodyVal=``
+
+let qqreadtimeurlVal=``
+
+let qqreadtimeheaderVal=``
+
+
+
+
+
+let qqreadcookie = {
+        qqreadbodyVal: qqreadbodyVal,
+        qqreadtimeurlVal: qqreadtimeurlVal,
+        qqreadtimeheaderVal: qqreadtimeheaderVal
+}
+
+module.exports =  qqreadcookie
+  
+
+
+
+
+
+
+
+
+
